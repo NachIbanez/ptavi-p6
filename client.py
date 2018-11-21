d9fe22dbe6016a070cc3a875b09ef6f9a7a67feb
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2018-11-21 13:45:39 +0100

    Cliente y servidor arreglado
    Falta comprobar el mp32rtp en el laboratorio y pasar pycodestyle

--- client.py
+++ client.py
@@ -19,18 +19,25 @@ try:
 except IndexError:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
+
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
-    
-    if METHOD == "INVITE":
+
+    print("\r\n--Enviando--\r\n" + LINE + "\r\n")
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+    message = data.decode('utf-8')
+    lista = (message.split())
+    print("--Recibido--\r\n" + message + "\r\n")
+    if lista == ['SIP/2.0', '100', 'Trying', 'SIP/2.0', '180', 'Ringing', 'SIP/2.0', '200', 'OK']:
+        LINE = ("ACK sip:" + sys.argv[2][:sys.argv[2].rfind(":")] + " SIP/2.0")
         print("Enviando: " + LINE)
         my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
         data = my_socket.recv(1024)
-    
-        print('Recibido -- ', data.decode('utf-8'))
-        print("Terminando socket...")
+
+    print("Terminando socket...")
 
 print("Fin.")
--- server.py
+++ server.py
@@ -5,6 +5,8 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -14,12 +16,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            if line[:line.find(" ")] == "INVITE":
-                print("El cliente nos manda " + line.decode('utf-8'))
+            line = line.decode('utf-8')
+            method = line[:line.find(" ")]
+            if method == "INVITE":
+                print("El cliente nos manda " + line)
+                if not "@" in line or not ":" in line:
+                    self.wfile.write(b'SIP/2.0 400 Bad Request')
+                else:
+                    self.wfile.write(b'SIP/2.0 100 Trying\r\n\r\n'
+                                     b'SIP/2.0 180 Ringing\r\n\r\n'
+                                     b'SIP/2.0 200 OK\r\n\r\n')
+            elif method == "ACK":
+                aEjecutar = './mp32rtp -i ' + IP + ' -p 23032 <' + AUDIO
+                os.system(aEjecutar)
+            elif line[:line.find(" ")] == "BYE":
+                print("El cliente nos manda " + line)
+                if not "@" in line or not ":" in line:
+                    self.wfile.write(b'SIP/2.0 400 Bad Request')
+                else:
+                    self.wfile.write(b'SIP/2.0 200 OK\r\n\r\n')
+            elif line and method != "INVITE" and method != "BYE" and method != "ACK":
+                self.wfile.write(b'SIP/2.0 405 Method Not Allowed')
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
@@ -27,6 +47,13 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
+    try:
+        IP = sys.argv[1]
+        PORT = sys.argv[2]
+        AUDIO = sys.argv[3]
+    except Index_Error:
+        sys.exit("Usage: python3 server.py IP port audio_file")
+
     serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    print("Listening")
     serv.serve_forever()
