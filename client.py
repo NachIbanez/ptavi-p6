36b349f7095820a5ad4135c2b8bd487ff3021215
NachIbanez <43443842+NachIbanez@users.noreply.github.com>
2018-11-21 17:51:45 +0100

    Pycodestyle comprobado y captura hecha
    trying ahora sale con body con los otros dos mensajes SIP en lugar de las líneas amarillas de error

--- client.py
+++ client.py
@@ -15,7 +15,8 @@ try:
     SIP_DIR = sys.argv[2][:sys.argv[2].find("@")]
     SERVER = sys.argv[2][sys.argv[2].find("@")+1:sys.argv[2].rfind(":")]
     PORT = int(sys.argv[2][sys.argv[2].rfind(":")+1:])
-    LINE = (METHOD + " sip:" + sys.argv[2][:sys.argv[2].rfind(":")] + " SIP/2.0")
+    LINE = (METHOD + " sip:" + sys.argv[2][:sys.argv[2].rfind(":")] +
+            " SIP/2.0")
 except IndexError:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
@@ -27,15 +28,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
 
     print("\r\n--Enviando--\r\n" + LINE + "\r\n")
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)
     message = data.decode('utf-8')
     lista = (message.split())
     print("--Recibido--\r\n" + message + "\r\n")
-    if lista == ['SIP/2.0', '100', 'Trying', 'SIP/2.0', '180', 'Ringing', 'SIP/2.0', '200', 'OK']:
+    if lista == ['SIP/2.0', '100', 'Trying', 'SIP/2.0', '180',
+                 'Ringing', 'SIP/2.0', '200', 'OK']:
         LINE = ("ACK sip:" + sys.argv[2][:sys.argv[2].rfind(":")] + " SIP/2.0")
         print("Enviando: " + LINE)
-        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
         data = my_socket.recv(1024)
 
     print("Terminando socket...")
--- server.py
+++ server.py
@@ -23,28 +23,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             method = line[:line.find(" ")]
             if method == "INVITE":
                 print("El cliente nos manda " + line)
-                if not "@" in line or not ":" in line:
+                if "@" not in line or ":" not in line:
                     self.wfile.write(b'SIP/2.0 400 Bad Request')
                 else:
-                    self.wfile.write(b'SIP/2.0 100 Trying\r\n'
-                                     b'SIP/2.0 180 Ringing\r\n'
-                                     b'SIP/2.0 200 OK\r\n')
+                    self.wfile.write(b'SIP/2.0 100 Trying\r\n\r\n'
+                                     b'SIP/2.0 180 Ringing\r\n\r\n'
+                                     b'SIP/2.0 200 OK\r\n\r\n')
             elif method == "ACK":
                 print("El cliente nos manda " + line)
                 aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + AUDIO
                 os.system(aEjecutar)
             elif line[:line.find(" ")] == "BYE":
                 print("El cliente nos manda " + line)
-                if not "@" in line or not ":" in line:
-                    self.wfile.write(b'SIP/2.0 400 Bad Request\r\n')
+                if "@" not in line or ":" not in line:
+                    self.wfile.write(b'SIP/2.0 400 Bad Request\r\n\r\n')
                 else:
                     self.wfile.write(b'SIP/2.0 200 OK\r\n')
-            elif line and method != "INVITE" and method != "BYE" and method != "ACK":
-                self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n')
+            elif line and method != "INVITE" and method != "BYE"\
+                    and method != "ACK":
+                self.wfile.write(b'SIP/2.0 405 Method Not Allowed\r\n\r\n')
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
